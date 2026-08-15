c4eb8eb889dd8747af31d738e1f23e6678015a6d
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2026-08-15 20:55:42 +0200

    update repository

--- auth-service/src/main/java/com/ocp/auth_service/repository/RoleRepository.java
+++ auth-service/src/main/java/com/ocp/auth_service/repository/RoleRepository.java
@@ -1,23 +1,21 @@
-package com.ocp.auth_service.Repository;
-
-import com.ocp.auth_service.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+package  com.ocp.auth_service.Repository;
 
 
-import java.util.List;
+
 import java.util.Optional;
 import java.util.UUID;
 
-public interface RoleRepository extends JpaRepository<Role, UUID> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-    Optional<Role> findByName (String name);
-    @Query(value = """
-        SELECT r.name AS roleName, COUNT(ur.user_id) AS userCount
-        FROM roles r
-        LEFT JOIN user_roles ur ON ur.role_id = r.id
-        GROUP BY r.name
-        """,nativeQuery = true)
-    List<RoleUserCountProjection> countUsersByRole();
 
+import com.ocp.auth_service.entity.Role;
+
+@Repository
+public interface RoleRepository
+	extends JpaRepository<Role, UUID> {
+
+	Optional<Role> findByName(String name);
+
+	boolean existsByName(String name);
 }
